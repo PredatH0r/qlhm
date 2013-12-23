80d940a5e6e3899345be7dae5f64262d9b97f592
hbeham <horst@beham.biz>
2013-12-23 01:23:33 +0100

    fixed wrong file content committed earlier

--- server/public/qlhmUserscriptRepository.js
+++ server/public/qlhmUserscriptRepository.js
@@ -1,26 +1,11 @@
-﻿// ==UserScript==
-// @id             qlhmUserscriptList@beham.biz
-// @name           Quake Live QLHM Userscript List
-// @version        1.0
-// @description    Provides a list of known Userscripts to Quake Live Hook Manager
-// @namespace      beham.biz
-// @homepage       
-// @author         PredatH0r
-// @include        http://*.quakelive.com/*
-// @exclude        http://*.quakelive.com/forum*
-// @run-at         document-end
-// ==/UserScript==
-
-test = "Horst";
-window.foo = "bar";
-HOOK_MANAGER.knownUserscripts = [
-  111519, // QLRank.com Display
-  114449, // Quake Live Stream Notifier
-  110327, // Quake Live Escaper
-  73076, // Quake Live New Alt Browser
-  120117, // QL Gametype Switcher
-  186527, // Quake Live left-hand-side Player List Popup
-  126719, // Quake Live Extended Stats
-  152168, // Quake Live ingame friend commands
-  96307 // Linkify Plus for Quake Live
+﻿HOOK_MANAGER.userscriptRepository = [
+  { id: 96307, name: "Linkify Plus for Quake Live" },
+  { id: 120117, name: "QL Gametype Switcher" },
+  { id: 111519, name: "QLRank.com Display" },
+  { id: 110327, name: "Quake Live Escaper" },
+  { id: 126719, name: "Quake Live Extended Stats" },
+  { id: 152168, name: "Quake Live ingame friend commands" },
+  { id: 186527, name: "Quake Live Left-Hand-Side Player List Popup" },
+  { id: 73076, name: "Quake Live New Alt Browser" },
+  { id: 114449, name: "Quake Live Stream Notifier" },
 ];
