338b3a19d01c9856c9ff962301d4781b4ba73388
supahgreg <greg.parris@gmail.com>
2014-01-05 13:06:08 -0600

    1. Check ".meta.js" before doing a full script retrieval (closes #1) 2. Break out userscripts.org stuff to "uso.js"

--- server/app.js
+++ server/app.js
@@ -2,22 +2,14 @@
 var util = require("util")
 
   , moment = require("moment")
-  , request = require("request")
   , restify = require("restify")
 
   , Scriptish_parser = require("./lib/scriptish").Scriptish_parser
+  , uso = require("./lib/uso")
   ;
 
 
 // Vars
-var uso = {
-    meta: {s: "https://userscripts.org/scripts/source/%d.meta.js", r: /^https?:\/\/userscripts\.org\/scripts\/source\/(\d{1,16})\.meta\.js/}
-  , script: {s: "https://userscripts.org/scripts/source/%d.user.js", r: /^https?:\/\/userscripts\.org\/scripts\/source\/(\d{1,16})\.user\.js/}
-};
-
-var RE_digits = /^\d{1,16}$/;
-var err404 = "Invalid or missing options.  '/uso/[id_number]' and '/uso/[full_url_to_.user.js]' are accepted.";
-
 var SCRIPT_CACHE = {};
 
 
@@ -26,6 +18,17 @@ function logTime() {
   console.log("%s %s", moment().format(), util.format.apply(null, arguments));
 }
 
+
+// Helper to update caching times for a script
+function updateMetaTimes(aScript, aNextMinutes) {
+  aScript.nextCheck = moment().add("minutes", aNextMinutes);
+  if (!aScript.body) return;
+  aScript.body._meta.nextCheckMinutes = aNextMinutes;
+  aScript.body._meta.lastCheck = Date.now();
+  aScript.body._meta.lastCheckUTC = moment.utc().format();
+  aScript.body._meta.nextCheck = aScript.nextCheck.valueOf();
+  aScript.body._meta.nextCheckUTC = aScript.nextCheck.utc();
+}
 
 // The latest "hook.js" version
 var LATEST_CLIENT_VERSION = 0.2
@@ -59,99 +62,113 @@ server.get({path: "/versioncheck", version: "1.0.0"}, function(req, res, next) {
 });
 
 server.get({path: /^\/uso\/?(.*)?/i, version: "1.0.0"}, function(req, res, next) {
-  var scriptID, scriptURL, reqID, reqURL;
+  var scriptID = uso.parseID(req.params[0]);
+  if (!scriptID) return res.send(404, {error: "Invalid or missing options.  '/uso/[id_number]' and '/uso/[full_url_to_.user.js]' are accepted."});
 
-  // trailing slugs
-  if (req.params[0]) {
-    // [id]
-    if (RE_digits.test(req.params[0]))
-      scriptID = req.params[0];
-    // [url]
-    else if (uso.script.r.test(req.params[0]))
-      scriptURL = req.params[0];
-  }
-  // nothing...
-  else {
-    return res.send(404, {error: err404});
-  }
+  // Create a cache entry if needed so we can always set "nextCheck"
+  if (!(scriptID in SCRIPT_CACHE)) SCRIPT_CACHE[scriptID] = {};
 
-  // Try to get request URL and script ID
-  if (scriptID) {
-    reqURL = util.format(uso.script.s, scriptID);
-    reqID = parseInt(scriptID);
-  }
-  else if (scriptURL) {
-    reqURL = scriptURL;
-    reqID = parseInt(uso.script.exec(scriptURL)[0]);
-  }
-
-  if (reqURL && reqID) {
-    // Create a cache entry if needed so we can always set "nextCheck"
-    if (!(reqID in SCRIPT_CACHE)) SCRIPT_CACHE[reqID] = {};
-
-    // Send from cache if available and we're not due for a refresh attempt
-    if (reqID in SCRIPT_CACHE && SCRIPT_CACHE[reqID].nextCheck && !moment().isAfter(SCRIPT_CACHE[reqID].nextCheck)) {
-      if (SCRIPT_CACHE[reqID].body) {
-        SCRIPT_CACHE[reqID].body._meta.nextCheckMinutes = SCRIPT_CACHE[reqID].nextCheck.diff(moment(), "minutes");
-        res.send(SCRIPT_CACHE[reqID].body);
-      }
-      else {
-        var tryAgainIn = moment.isMoment(SCRIPT_CACHE[reqID].nextCheck) ? util.format("  You can try again %s.", SCRIPT_CACHE[reqID].nextCheck.fromNow()) : ""
-          , errMsg = util.format("Previous request for script with ID %d failed.%s", reqID, tryAgainIn)
-          ;
-        logTime(errMsg);
-        res.send(403, { error: errMsg });
-      }
+  // Send from cache if available and we're not due for a refresh attempt
+  if (scriptID in SCRIPT_CACHE && SCRIPT_CACHE[scriptID].nextCheck && !moment().isAfter(SCRIPT_CACHE[scriptID].nextCheck)) {
+    if (SCRIPT_CACHE[scriptID].body) {
+      // Only update the decreasing number of minutes
+      SCRIPT_CACHE[scriptID].body._meta.nextCheckMinutes = SCRIPT_CACHE[scriptID].nextCheck.diff(moment(), "minutes");
+      res.send(SCRIPT_CACHE[scriptID].body);
     }
-    // Otherwise try a request from USO
     else {
-      logTime("Performing a new request for script %d (%s)", reqID, reqURL);
-      request({url: reqURL, timeout: 10E3}, function(usoError, usoResponse, usoBody) {
-        if (404 === usoResponse.statusCode) {
-          logTime("404 received for script %d (%s)", reqID, reqURL);
-          // Wait 60 minutes if USO said it couldn't be found
-          SCRIPT_CACHE[reqID].nextCheck = moment().add("minutes", 60);
-          res.send(404, {error: util.format("Script with ID %d was not found", reqID)});
-        }
-        else if (usoError) {
-          logTime("Non-404 error for script %d (%s): %s", reqID, reqURL, usoError);
-          // Wait 5 minutes if there was a non-404 error in retrieval
-          SCRIPT_CACHE[reqID].nextCheck = moment().add("minutes", 5);
-          res.send(404, {error: "Invalid request"});
+      var tryAgainIn = moment.isMoment(SCRIPT_CACHE[scriptID].nextCheck) ? util.format("  You can try again %s.", SCRIPT_CACHE[scriptID].nextCheck.fromNow()) : ""
+        , errMsg = util.format("Previous request for script with ID %d failed.%s", scriptID, tryAgainIn)
+        ;
+      logTime(errMsg);
+      res.send(403, { error: errMsg });
+    }
+  }
+  // Otherwise try a request from USO
+  else {
+    logTime("Performing a new metadata request for script %d", scriptID);
+
+    uso.getMeta(scriptID, function(aError, aHeaders) {
+      if (404 === aError) {
+        logTime("404 received for script metadata %d", scriptID);
+        // Wait 60 minutes if USO said the metadata couldn't be found
+        updateMetaTimes(SCRIPT_CACHE[scriptID], 60);
+        return res.send(404, {error: util.format("Metadata for script with ID %d was not found", scriptID)});
+      }
+      else if (aError) {
+        logTime("Non-404 error for script metadata %d: %s", scriptID, aError);
+        // Wait 5 minutes if there was a non-404 error in retrieval
+        updateMetaTimes(SCRIPT_CACHE[scriptID], 5);
+
+        // Send the previous body if available
+        if (SCRIPT_CACHE[scriptID].body) return res.send(SCRIPT_CACHE[scriptID].body);
+        return res.send(404, {error: "Invalid metadata request"});
+      }
+
+      // Successful metadata retrieval... check if script is cached, and if metadata version is different
+      var scriptName = aHeaders.name ? aHeaders.name[0] : "unspecified";
+      logTime("Successful metadata retrieval for script %d (\"%s\")", scriptID, scriptName);
+
+      if (!SCRIPT_CACHE[scriptID].body
+          || SCRIPT_CACHE[scriptID].body._meta["uso:version"] !== aHeaders["uso:version"][0]) {
+        if (!SCRIPT_CACHE[scriptID].body) {
+          logTime("Script %d (\"%s\") is not cached.  Requesting full script (\"uso:version\" %d)...",
+              scriptID, scriptName, aHeaders["uso:version"]);
         }
         else {
-          var scriptHeaders = Scriptish_parser(usoBody)
-            , scriptName = scriptHeaders.name ? scriptHeaders.name[0] : "unspecified"
-            ;
-          logTime("Successful response for script %d (\"%s\", %s)", reqID, scriptName, reqURL);
+          logTime("New version detected for script %d (\"%s\", old: \"%s\", new: \"%s\").  Requesting full script...",
+              scriptID, scriptName, SCRIPT_CACHE[scriptID].body._meta["uso:version"], aHeaders["uso:version"]);
+        }
+
+        // Attempt a full update
+        uso.getScript(scriptID, function(aError, aResult) {
+          if (404 === aError) {
+            logTime("404 received for script %d", scriptID);
+            // Wait 60 minutes if USO said the script couldn't be found
+            updateMetaTimes(SCRIPT_CACHE[scriptID], 60);
+            return res.send(404, {error: util.format("Script with ID %d was not found", scriptID)});
+          }
+          else if (aError) {
+            logTime("Non-404 error for script %d: %s", scriptID, aError);
+            // Wait 5 minutes if there was a non-404 error in retrieval
+            updateMetaTimes(SCRIPT_CACHE[scriptID], 5);
+            // Send the previous body if available
+            if (SCRIPT_CACHE[scriptID].body) return res.send(SCRIPT_CACHE[scriptID].body);
+            return res.send(404, {error: "Invalid script request"});
+          }
+
+          // Successful new script retrieval... update the cache and send it along
+          logTime("Successful response for script %d (\"%s\")", scriptID, scriptName);
 
           // Clear the cache entry
-          SCRIPT_CACHE[reqID] = {};
+          SCRIPT_CACHE[scriptID] = {};
 
           // Good response.  Cache the data and send it along.
-          // Wait 30 minutes for the next recache.
-
-          SCRIPT_CACHE[reqID].nextCheck = moment().add("minutes", 30);
-          SCRIPT_CACHE[reqID].body = {
-              "_meta": {
-                  id: reqID
-                , lastCheck: Date.now()
-                , lastCheckUTC: moment.utc().format()
-                , nextCheck: SCRIPT_CACHE[reqID].nextCheck.valueOf()
-                , nextCheckUTC: SCRIPT_CACHE[reqID].nextCheck.utc()
-                , nextCheckMinutes: SCRIPT_CACHE[reqID].nextCheck.diff(moment(), "minutes")
-              }
-            , headers: scriptHeaders
-            , content: usoBody
+          SCRIPT_CACHE[scriptID].body = {
+              "_meta": { id: scriptID }
+            , headers: aResult.headers
+            , content: aResult.content
           };
-          res.send(SCRIPT_CACHE[reqID].body);
-        }
-      });
-    }
-  }
-  // Missing URL and/or ID
-  else {
-    return res.send(404, {error: err404});
+
+          // Add useful metadata headers starting with "uso:"
+          for (var i in aHeaders) {
+            if (0 === i.indexOf("uso:")) SCRIPT_CACHE[scriptID].body._meta[i] = aHeaders[i][0];
+          }
+
+          // Wait 30 minutes for the next recache.
+          updateMetaTimes(SCRIPT_CACHE[scriptID], 2);
+
+          res.send(SCRIPT_CACHE[scriptID].body);
+        });
+      }
+      // Cached script is still okay... update times and send it along
+      else {
+        logTime("The cached version of script %d (\"%s\", \"uso:version\": \"%s\") is the latest available.",
+            scriptID, scriptName, aHeaders["uso:version"][0]);
+        // Wait 30 minutes for the next recache.
+        updateMetaTimes(SCRIPT_CACHE[scriptID], 2);
+        res.send(SCRIPT_CACHE[scriptID].body);
+      }
+    });
   }
 });
 
---
+++ server/lib/uso.js
@@ -0,0 +1,62 @@
+var request = require("request")
+  , util = require("util")
+
+  , Scriptish_parser = require("./scriptish").Scriptish_parser
+  ;
+
+var uso = Object.create(null)
+  , meta = {str: "https://userscripts.org/scripts/source/%d.meta.js", reg: /^https?:\/\/userscripts\.org\/scripts\/source\/(\d{1,16})\.meta\.js/}
+  , script = {str: "https://userscripts.org/scripts/source/%d.user.js", reg: /^https?:\/\/userscripts\.org\/scripts\/source\/(\d{1,16})\.user\.js/}
+  , RE_digits = /^\d{1,16}$/
+  , DOLITTLE = function() {}
+  ;
+
+
+/**
+ * Return the integer script ID or false
+ *
+ * NOTE: we know the parseInt result will be okay due to the meta/script regex
+ */
+uso.parseID = function(aStr) {
+  if (aStr) {
+    // ID
+    if (RE_digits.test(aStr)) return parseInt(aStr);
+    // Script URL
+    else if (script.reg.test(aStr)) return parseInt(uso.script.exec(aStr)[0]);
+    // Meta URL
+    else if (meta.reg.test(aStr)) return parseInt(uso.script.exec(aStr)[0]);
+  }
+  return false;
+}
+
+
+/**
+ * Try to retrieve and parse a userscripts.org meta file
+ */
+uso.getMeta = function(aID, aCallback) {
+  aCallback = aCallback ? aCallback : DOLITTLE;
+  var url = util.format(meta.str, aID);
+
+  request({url: url, timeout: 10E3}, function(usoError, usoResponse, usoBody) {
+    if (404 === usoResponse.statusCode) return aCallback(404);
+    if (usoError) return aCallback(usoError);
+    aCallback(null, Scriptish_parser(usoBody));
+  });
+}
+
+/**
+ * Try to retrieve and parse a userscripts.org meta file
+ */
+uso.getScript = function(aID, aCallback) {
+  aCallback = aCallback ? aCallback : DOLITTLE;
+  var url = util.format(script.str, aID);
+
+  request({url: url, timeout: 10E3}, function(usoError, usoResponse, usoBody) {
+    if (404 === usoResponse.statusCode) return aCallback(404);
+    if (usoError) return aCallback(usoError);
+    aCallback(null, {headers: Scriptish_parser(usoBody), content: usoBody});
+  });
+}
+
+// Export "uso" as the module
+module.exports = uso;
